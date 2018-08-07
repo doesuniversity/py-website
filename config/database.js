b73b98e82be91db287bc343a96e3f4441dff49de
Does University <doesprogrammer@gmail.com>
2018-08-07 15:48:54 +0700

    edit readme

---
+++ config/database.js
@@ -0,0 +1,7 @@
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'does'
+app.config['MYSQL_PASSWORD'] = '1'
+app.config['MYSQL_DB'] = 'flaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+
